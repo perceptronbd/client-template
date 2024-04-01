630eece39eb93909df9e8267b2391f13d729d5e0
asif-bhuti <asifaslam.aa81@gmail.com>
2024-04-01 16:43:11 +0600

    Add devtools-detector package and create DevtoolsStatus component and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import { Button } from './components';
+import DevtoolsStatus from './components/devtool/devtoolDetector';
+import useDevtoolsStatus from './hooks/useDevtoolsStatus';
 
 function App() {
+  // const navigate = () => {
+  //   window.location.href = 'https://www.google.com';
+  // };
+
+  // const handleDevtoolsOpen = () => {
+  //   // console.log('DevTools are open!');
+  //   navigate();
+  //   // You can perform any action here, such as logging, setting state, etc.
+  // };
+
+  // Use the custom hook
+  // useDevtoolsStatus(handleDevtoolsOpen);
+
   return (
     <div className="bg-red-300">
-      <Button>This is a button</Button>
+      {/* <DevtoolsStatus /> */}
+      <Button>Text</Button>
     </div>
   );
 }
