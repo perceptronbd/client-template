483776d95e1ee3b42373693dd0f05ede79d1bbaa
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-20 18:58:45 +0600

    add re-usable inputs to input page

--- src/pages/home/input/InputPage.jsx
+++ src/pages/home/input/InputPage.jsx
@@ -4,8 +4,20 @@ import { FormInput } from '../../../components/input/FormInput';
 import { ImgInput } from '../../../components/input/ImgInput';
 import { Input } from '../../../components/input/Input';
 import { SearchInput } from '../../../components/input/SearchInput';
+import { SelectInput } from '../../../components/input/SelectInput';
+import { TextInput } from '../../../components/input/TextInput';
 
 const InputPage = () => {
+  const selectOpts = {
+    fruits: [
+      { value: 'apple', label: 'Apple' },
+      { value: 'banana', label: 'Banana' },
+    ],
+    vegetables: [
+      { value: 'aubergine', label: 'Aubergine' },
+      { value: 'broccoli', label: 'Broccoli' },
+    ],
+  };
   return (
     <div className="grid gap-6">
       <Checkbox id="checkbox1" label="Checkbox" />
@@ -13,6 +25,8 @@ const InputPage = () => {
       <ImgInput />
       <Input placeholder="Input" />
       <SearchInput />
+      <SelectInput selectOpts={selectOpts} placeholder="Select Input" />
+      <TextInput label="Text Input" />
     </div>
   );
 };
