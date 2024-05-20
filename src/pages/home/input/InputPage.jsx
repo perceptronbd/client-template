cb7dd3400c874dcb0571666c5a39b8668d5201d1
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-05-20 18:47:38 +0600

    added re-usable img input

--- src/pages/home/input/InputPage.jsx
+++ src/pages/home/input/InputPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Checkbox } from '../../../components/input/Checkbox';
 import { FormInput } from '../../../components/input/FormInput';
+import { ImgInput } from '../../../components/input/ImgInput';
 
 const InputPage = () => {
   return (
     <div className="grid gap-6">
       <Checkbox id="checkbox1" label="Checkbox" />
       <FormInput placeholder="Form Input" />
+      <ImgInput />
     </div>
   );
 };
