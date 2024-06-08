6d5a81452fa683aba5f855ba8186f45c748b6840
Amdadul Islam <amdadullahrayhan@gmail.com>
2024-06-08 22:57:11 +0600

    Refactor TableSkeleton component for improved readability and maintainability

---
+++ src/components/skeletons/TableSkeleton.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '../container/Container';
+
+const TableSkeleton = () => {
+  return (
+    <Container className="grid grid-cols-6 grid-rows-6 gap-2 p-4 bg-white">
+      <div className="h-10 col-start-1 row-start-1 rounded-md w-72 animate-pulse bg-neutral-200" />
+      {[...Array(11)].map((_, index) => (
+        <div key={index} className="h-10 col-span-5 rounded-md animate-pulse bg-neutral-200" />
+      ))}
+    </Container>
+  );
+};
+
+export default TableSkeleton;
